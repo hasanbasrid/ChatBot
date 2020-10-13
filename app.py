c0433595e64308ff5e7aa3bd85a7cf7f677b5de7
Hasan Dulgeroglu <hbd3>
2020-10-13 20:27:17 +0000

    Remove database related code for now and replace it with a list variable. Fix names of fields

--- app.py
+++ app.py
@@ -7,36 +7,19 @@ import flask_sqlalchemy
 import flask_socketio
 import models 
 
-ADDRESSES_RECEIVED_CHANNEL = 'addresses received'
+MESSAGES_RECEIVED_CHANNEL = 'messages received'
 
 app = flask.Flask(__name__)
 
 socketio = flask_socketio.SocketIO(app)
 socketio.init_app(app, cors_allowed_origins="*")
 
-dotenv_path = join(dirname(__file__), 'sql.env')
-load_dotenv(dotenv_path)
+all_messages = []
 
-sql_user = os.environ['SQL_USER']
-sql_pwd = os.environ['SQL_PASSWORD']
-dbuser = os.environ['USER']
-
-database_uri = 'postgresql://{}:{}@localhost/postgres'.format(
-    sql_user, sql_pwd)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
-
-db = flask_sqlalchemy.SQLAlchemy(app)
-db.init_app(app)
-db.app = app
-
-
-db.create_all()
-db.session.commit()
-
-def emit_all_addresses(channel):
-    # TODO
-    print("TODO")
+def emit_all_messages(channel):
+    socketio.emit(channel, {
+        'allMessages' : all_messages
+    })
 
 @socketio.on('connect')
 def on_connect():
@@ -45,7 +28,7 @@ def on_connect():
         'test': 'Connected'
     })
     
-    # TODO
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
     
 
 @socketio.on('disconnect')
@@ -55,15 +38,12 @@ def on_disconnect():
 @socketio.on('new address input')
 def on_new_address(data):
     print("Got an event for new address input with data:", data)
-    
-    db.session.add(models.Usps(data["address"]));
-    db.session.commit();
-    
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
+    all_messages.append(data)
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
 @app.route('/')
 def index():
-    emit_all_addresses(ADDRESSES_RECEIVED_CHANNEL)
+    emit_all_messages(MESSAGES_RECEIVED_CHANNEL)
 
     return flask.render_template("index.html")
 
