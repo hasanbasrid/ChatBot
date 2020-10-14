a81a13243a02cb93cdbf4e8c67bfb15b45dd4f2e
Hasan Dulgeroglu <hbd3>
2020-10-14 01:04:14 +0000

    Add new bot commands that  use funtranstlate API to translate to leetspeak and 8ball API to answer questions

--- bot.py
+++ bot.py
@@ -1,15 +1,34 @@
 import requests
+import json
+import json
+import urllib.parse
 
 def command(text):
+    split_text = text.split(' ',2)
     
-    
-    if text[:2] != "!!":
+    if split_text[0] != "!!":
         return ""
+    
+    cmd = split_text[1]
+    
+    if cmd == "help":
+        return "You can use these commands in the format '!! <command> <message>': about - help - funtranslate - 8ball - "
+    
+    elif cmd == "about":
+        return "I'm a bot that answers to '!! <command> <message>'. Type '!! help' to learn more about commands"
+    
+    elif cmd == "funtranslate":
+        payload = {'text' : split_text[2]}
+        res = requests.get("https://api.funtranslations.com/translate/leetspeak.json", params = payload)
+        return json.loads(res.text)['contents']['translated']
         
-    command = text.split()[1]
-    
-    if command == "help":
-        return "You can use these commands :<br> help - <br>funtranslate - <br>8ball - <br>"
-    
-    if command == "about":
+        
+    elif cmd == "8ball":
+        question = urllib.parse.quote(split_text[2])
+        res = requests.get("https://8ball.delegator.com/magic/JSON/" + question)
+        return json.loads(res.text)['magic']['answer']
+        
+    elif cmd == "TODO":
         return ""
+    else:
+        return "Command not recognized. Type '!! help' for list of commands"
