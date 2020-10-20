05e22597934c38a8bafd6490aa5aab69fb7970d7
Hasan Dulgeroglu <hbd3>
2020-10-20 21:06:10 +0000

    Add users database

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from app import db
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender = db.Column(db.String(60))
+    sender = db.Column(db.String())
     message = db.Column(db.String())
     
     def __init__(self, usr, msg):
@@ -14,3 +14,17 @@ class Chat(db.Model):
         
     def __repr__(self):
         return '<Message: %s>' % self.message
+        
+
+class Users(db.Model):
+    email = db.Column(db.String())
+    name = db.Column(db.String())
+    profile_pic = db.Column(db.String())
+    
+    def __init__(self, email, name, profile_pic):
+        self.email = email
+        self.name = name
+        self.profile_pic = profile_pic
+    
+    def __repr__(self):
+        return '<User: profilepic: {} name : {} email: {}>'.format(self.profile_pi, self.email, self.name)
